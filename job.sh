d78cab72f025186cfab7beaf1721440b5cc2ee5b
greyspectrum <alsmith@g.harvard.edu>
2018-07-19 10:46:50 -0400

    Initial commit.

---
+++ job.sh
@@ -0,0 +1,5 @@
+#!/usr/env/bin bash
+
+git clone https://github.com/greyspectrum/inception.git
+cd inception
+python mission.py
---
+++ mission.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+
+string = '''\nFLASH PRIORITY ONE TELEX:
+
+STRAP 1 COMINT NOFORN
+=====================
+
+A client has contacted us with a request that requires a
+very particular set of skills. The client has emphasized
+the necessity of discretion in this matter.
+
+Given the sensitivity of the client's request, we naturally
+thought of your team.
+
+Below, you will see find a list of IP addresses. These
+machines collectively store secret information that is of
+interest to our client. We have reason to beleive that this
+confidential information has been encoded and sharded
+across these three machines, in order to thwart exfiltration.
+
+In order to recieve payment for your services, you
+must access these secure machines, extract the secrets,
+assemble them, and decode the information.
+
+As usual, any contractual relationship with your team
+will be publicly denied, should you be compromised.
+
+SYSTEM INFORMATION:
+
+10.242.118.30
+
+10.242.118.31
+
+10.242.118.32
+
+GOOD LUCK.
+\n'''
+
+for char in string:
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(.025)
